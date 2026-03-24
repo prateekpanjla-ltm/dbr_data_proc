d2da4f51162ca62b4b0e0377f41a31e4119b82ec
prateek.panjla@ltimindtree.com <prateek.panjla@ltimindtree.com>
2026-03-24 14:44:27 +0000

    permissions

--- infra/setup-dev.sql
+++ infra/setup-dev.sql
@@ -7,5 +7,11 @@ CREATE CATALOG IF NOT EXISTS gharchive_dev;
 CREATE SCHEMA IF NOT EXISTS gharchive_dev.pipeline;
 CREATE VOLUME IF NOT EXISTS gharchive_dev.pipeline.raw_files;
 
+-- Grant workspace users access
+GRANT USE CATALOG ON CATALOG gharchive_dev TO `account users`;
+GRANT USE SCHEMA ON SCHEMA gharchive_dev.pipeline TO `account users`;
+GRANT READ VOLUME, WRITE VOLUME ON VOLUME gharchive_dev.pipeline.raw_files TO `account users`;
+GRANT CREATE TABLE ON SCHEMA gharchive_dev.pipeline TO `account users`;
+
 -- Verify
 SHOW VOLUMES IN gharchive_dev.pipeline;
--- infra/setup-test.sql
+++ infra/setup-test.sql
@@ -7,5 +7,11 @@ CREATE CATALOG IF NOT EXISTS gharchive_test;
 CREATE SCHEMA IF NOT EXISTS gharchive_test.pipeline;
 CREATE VOLUME IF NOT EXISTS gharchive_test.pipeline.raw_files;
 
+-- Grant workspace users access
+GRANT USE CATALOG ON CATALOG gharchive_test TO `account users`;
+GRANT USE SCHEMA ON SCHEMA gharchive_test.pipeline TO `account users`;
+GRANT READ VOLUME, WRITE VOLUME ON VOLUME gharchive_test.pipeline.raw_files TO `account users`;
+GRANT CREATE TABLE ON SCHEMA gharchive_test.pipeline TO `account users`;
+
 -- Verify
 SHOW VOLUMES IN gharchive_test.pipeline;
